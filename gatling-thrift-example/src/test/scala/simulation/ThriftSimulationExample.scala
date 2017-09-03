fc80d0bf9725840c219f208367ae964dddd3fe19
Yusuke Izawa <yuizalp@gmail.com>
2017-09-04 01:14:17 +0900

    Improve ThriftProtocol DSL

--- gatling-thrift-example/src/test/scala/simulation/ThriftSimulationExample.scala
+++ gatling-thrift-example/src/test/scala/simulation/ThriftSimulationExample.scala
@@ -5,7 +5,7 @@ import com.twitter.util.Future
 import io.gatling.core.Predef._
 import io.gatling.core.structure.ScenarioBuilder
 import io.gatling.thrift.Predef._
-import io.gatling.thrift.data.Connection
+import io.gatling.thrift.protocol.ThriftProtocol
 import org.micchon.ping.thriftscala.PingService
 
 import scala.util.Random
@@ -15,10 +15,11 @@ class ThriftSimulationExample extends ThriftSimulation {
   val client: PingService.FutureIface =
     Thrift.client.newIface[PingService.FutureIface]("localhost:9911")
 
-  implicit val connection = Connection("localhost", 9911)
-
   implicit val callback: Future[String] =
     client.echo(new Random().nextInt().toString)
+
+  implicit val thriftProtocol: ThriftProtocol =
+    thrift.port(9991).host("localhost").requestName("example request")
 
   val scn: ScenarioBuilder = scenario("Thrift Scenario").repeat(2) {
     exec(callback)
--- gatling-thrift/src/main/scala/io/gatling/thrift/protocol/ThriftProtocolBuilder.scala
+++ gatling-thrift/src/main/scala/io/gatling/thrift/protocol/ThriftProtocolBuilder.scala
@@ -3,8 +3,12 @@ package io.gatling.thrift.protocol
 import io.gatling.thrift.data._
 
 case class ThriftProtocolBuilder(connection: Connection = Connection()) {
-  def host(h: String) = copy(connection = connection.copy(host = h))
-  def port(p: Int) = copy(connection = connection.copy(port = p))
+  def host(h: String): ThriftProtocolBuilder =
+    copy(connection = connection.copy(host = h))
+  def port(p: Int): ThriftProtocolBuilder =
+    copy(connection = connection.copy(port = p))
+  def requestName(r: String): ThriftProtocolBuilder =
+    copy(connection = connection.copy(requestName = r))
 
   def build(): ThriftProtocol = {
     connection.validate()
