c10e2d5676c792f124a99331d4c47f8881c61f4c
Yusuke Izawa <yuizalp@gmail.com>
2017-09-11 21:10:54 +0900

    Add a new callback for #9

--- gatling-thrift-example/src/test/scala/simulation/ThriftSimulationExample.scala
+++ gatling-thrift-example/src/test/scala/simulation/ThriftSimulationExample.scala
@@ -1,5 +1,7 @@
 package simulation
 
+import java.util.concurrent.atomic.AtomicBoolean
+
 import com.twitter.finagle.Thrift
 import com.twitter.util.Future
 import io.gatling.core.Predef._
@@ -15,15 +17,27 @@ class ThriftSimulationExample extends ThriftSimulation {
   val client: PingService.FutureIface =
     Thrift.client.newIface[PingService.FutureIface]("localhost:9911")
 
-  def callback: Future[String] =
+  private val isDone = new AtomicBoolean(false)
+
+  // https://github.com/3tty0n/gatling-thrift/issues/9
+  // This function arises OK once and makes rest of them KO
+  def callback: Future[String] = {
+    if (isDone.compareAndSet(false, true)) {
+      Future.value("first")
+    } else {
+      Future.exception(new RuntimeException("already done"))
+    }
+  }
+
+  def callbackSimple: Future[String] =
     client.echo(new Random().nextInt().toString)
 
   implicit val thriftProtocol: ThriftProtocol =
     thrift.port(9911).host("localhost").requestName("example request")
 
-  val scn: ScenarioBuilder = scenario("Thrift Scenario").repeat(2) {
-    exec(callback)
-  }
+  val scn: ScenarioBuilder = scenario("Thrift Scenario")
+    .exec(callback)
+    .exec(callbackSimple)
 
   setUp(scn.inject(nothingFor(4 seconds), atOnceUsers(100)))
 }
